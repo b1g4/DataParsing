ea9eb030038ae7ce59602e4122577d066fc5e87e
gch04721 <32638465+gch04721@users.noreply.github.com>
2019-04-30 04:06:18 +0900

    file create

---
+++ readData.py
@@ -0,0 +1,4 @@
+# Data per month
+# split by ';'
+# index use_mon | bus_route_id | bus_route_no | bus_route_nm | stnd_bsst_id | bsst_ars_no | bus_sta_id | bus_sta_nm | 승차, 하자[0~23] | dump | dump
+# route id를 API에서 사용하지 못하는 문제가 있음 저장 x
