97f1a3920587801806c3e98f612f87c19ac26f5e
gch04721 <jwhuh@uclab.re.kr>
2019-06-21 22:56:30 +0900

    merge complete2

--- OSSWProj/src/app/App.java
+++ OSSWProj/src/app/App.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.util.ArrayList;
+import bus.*;
 
 // import java.util.ArrayList;
 
@@ -14,23 +15,6 @@ public class App {
         Runtime runtime = Runtime.getRuntime();
         int mb = 1024*1024;
 
-        //routecsv.csv, stationcsv.csv, congestioncsv.csv 를 만드는 코드
-        /*
-        BusInfoClass busInfo = BusInfoClass.getInstance();
-        String currentPath = System.getProperty("user.dir");
-
-        String fileDir1 = currentPath + "\\OSSWProj\\20190124기준_서울시_버스노선정보_new.xls";
-        ParseFileClass parse1 = new ParseFileClass(fileDir1, true, true);
-
-        String fileDir2 = currentPath + "\\OSSWProj\\20190124기준_서울시_노선현황_첫차막차배차_new.xls";
-        ParseFileClass parse2 = new ParseFileClass(fileDir2, 0);
-
-        String fileDir3 = currentPath + "\\OSSWProj\\data\\bus";
-        ParseFileClass parse3= new ParseFileClass(fileDir3, false, true);
-        */
-
-       //routecsv.csv, stationcsv.csv, congestioncsv.csv 를 읽는 코드
-       /*
        BusInfoClass busInfo = BusInfoClass.getInstance();
        String currentPath = System.getProperty("user.dir");
 
@@ -42,7 +26,6 @@ public class App {
 
        String congestioncsvDir= currentPath+"\\congestioncsv.csv";
        ParseFileClass parse_congestion= new ParseFileClass(congestioncsvDir,"congestion", true);
-        */
 
         //파일3개를 미리 읽고 정보를 저장한 상태로 client를 기다려야 한다
         TCPServer tcpServer=new TCPServer();
